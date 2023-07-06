9f396322007e0fcfbb1c54c0bab30578815f94bd
Rimriru <ororonya@gmail.com>
2023-07-06 14:59:23 +0200

    fix: port

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -94,7 +94,7 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: "http://localhost:4000",
+  baseUrl: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
